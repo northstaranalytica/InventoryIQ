643cb39cbc29372b1b1515d2513e2a3d0e0648fc
Steve <meet_a_better_self@163.com>
2025-03-10 18:28:07 +0800

    Initial Commit

---
+++ GoodsAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodsAI
+//
+//  Created by Steve on 2025/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
